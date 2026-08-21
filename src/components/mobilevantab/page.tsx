f88ad655790676f05fd7479205486f4096074d1c
globifydeveloper-star <globifydeveloper@gmail.com>
2026-08-21 18:48:41 +0530

    feat: implement dynamic Footer component and mobile navigation tab layout

--- src/components/mobilevantab/page.tsx
+++ src/components/mobilevantab/page.tsx
@@ -1,10 +1,8 @@
-import Navbar from '@/components/layout/Navbar';
 import MobileVanHero from '@/components/mobilevantab/mobilevanhero/mobilevanhero';
 import FourStep from '@/components/mobilevantab/fourstep/fourstep';
 import TestingCards from '@/components/mobilevantab/testingcards/testingcards';
 import LocationService from '@/components/mobilevantab/locationservice/locationservice';
 import Appoinment from '@/components/mobilevantab/appoinment/appoinment';
-import Footer from '@/components/layout/Footer';
 import { MobileVanPageData } from '@/lib/strapi';
 
 interface MobileVanTabProps {
@@ -14,9 +12,6 @@ interface MobileVanTabProps {
 export default function MobileVanTab({ data }: MobileVanTabProps) {
   return (
     <main>
-      {/* 1. Navbar Navigation */}
-      <Navbar />
-
       {/* Mobile Van Hero Section */}
       <MobileVanHero data={data} />
 
@@ -31,9 +26,6 @@ export default function MobileVanTab({ data }: MobileVanTabProps) {
 
       {/* Book Your Van Appointment */}
       <Appoinment data={data} />
-
-      {/* 2. Footer Section */}
-      <Footer />
     </main>
   )
 }
